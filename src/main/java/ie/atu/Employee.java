859fbc28ecfe4b367ffc140941df08e47f55fe42
Iwo <g00391231@atu.ie>
2022-12-07 12:27:16 +0000

    Added Employee ID feature and test

--- src/main/java/ie/atu/Employee.java
+++ src/main/java/ie/atu/Employee.java
@@ -3,13 +3,15 @@ package ie.atu;
 public class Employee {
 
     String name;
+    String ID;
 
-    Employee(String name)
+    Employee(String name, String ID)
     {
-        this.name = getName(name);
+        this.name = setName(name);
+        this.ID = setID(ID);
     }
 
-    public String getName(String i) {
+    public String setName(String i) {
         if (i.length() >= 5 && i.length() <= 22)
         {
             return i;
@@ -17,6 +19,17 @@ public class Employee {
         else
         {
             throw new IllegalArgumentException("Name has to have minimum 5 characters and Maximum 22");
+        }
+    }
+
+    public String setID(String s) {
+        if (s.length() == 6)
+        {
+            return s;
+        }
+        else
+        {
+            throw new IllegalArgumentException("6 characters in length otherwise invalid PPS number error");
         }
     }
 }
--- src/test/java/ie/atu/EmployeeTest.java
+++ src/test/java/ie/atu/EmployeeTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @DisplayName("Employee Test")
 class EmployeeTest {
 
-    Employee Tester = new Employee("Aaron");
+    Employee Tester = new Employee("Aaron", "123456");
     @BeforeEach
    void beforeEach()
     {}
@@ -15,13 +15,25 @@ class EmployeeTest {
     @Test
     void nameTest()
     {
-        assertEquals("Aaron", Tester.getName("Aaron"));
+        assertEquals("Aaron", Tester.setName("Aaron"));
     }
     @Test
     void nameTestFail()
     {
-        Exception e_message = assertThrows(IllegalArgumentException.class, () -> new Employee("Joel"));
+        Exception e_message = assertThrows(IllegalArgumentException.class, () -> new Employee("Joel", "1234356"));
         assertEquals("Name has to have minimum 5 characters and Maximum 22", e_message.getMessage());
+    }
+
+    @Test
+    void IDTest()
+    {
+        assertEquals("123456", Tester.setID("123456"));
+    }
+    @Test
+    void IDTestFail()
+    {
+        Exception e_message = assertThrows(IllegalArgumentException.class, () -> new Employee("Joels","12345"));
+        assertEquals("6 characters in length otherwise invalid PPS number error", e_message.getMessage());
     }
 
     @AfterEach
